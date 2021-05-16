7c83d1d84fdcdc8ebec42a4c8582ce0c7d8078ff
Alizée Gillouaye <agillouaye@icloud.com>
2021-05-16 21:26:06 +0200

    api

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280"
 
+const getInfosOfMovie = (movie_id) => {
+    const INFOS_MOVIE = "https://api.themoviedb.org/3/movie/" + movie_id + "?api_key=942799d67633b6990a5f42060868ce66"
+    fetch(INFOS_MOVIE)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+        })
+}
+
 const setVoteClass = (vote) => {
     if(vote >= 8){
         return "green"
@@ -12,7 +21,16 @@ const setVoteClass = (vote) => {
     }
 }
 
-const Movie = ({title, poster_path, overview, vote_average}) => (
+const handleClick = (e, movie_id) => {
+    const INFOS_MOVIE = "https://api.themoviedb.org/3/movie/" + movie_id + "?api_key=942799d67633b6990a5f42060868ce66"
+    fetch(INFOS_MOVIE)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+        })
+}
+
+const Movie = ({title, poster_path, overview, vote_average, id}) => (
     <div className="movie">
         <img src={poster_path ? IMG_API + poster_path : "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1640&q=80"} alt={title} />
         <div className="movie-info">
@@ -23,6 +41,7 @@ const Movie = ({title, poster_path, overview, vote_average}) => (
         <div className="movie-over">
             <h2>Overview :</h2>
             <p>{overview}</p>
+            <button onClick={((e) => handleClick(e, id))}>Get Infos</button>
         </div>
     </div>
 )
